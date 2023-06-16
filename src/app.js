2554938dcf10402f409b84db55051781ca0d2473
Juan Cueva <juancuevac97@gmail.com>
2023-06-15 21:55:46 -0500

    feat: Añadir handlebars

--- src/app.js
+++ src/app.js
@@ -1,13 +1,32 @@
 import express from 'express';
+import handlebars from 'express-handlebars'
+
 import products from './routes/products.router.js';
 import carts from './routes/carts.router.js';
+import views from './routes/views.router.js';
+import __dirname from './utils.js';
+import { Server } from 'socket.io'
 
 const app = express();
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.engine('handlebars', handlebars.engine());
+app.set('views', __dirname + '/views');
+app.set('view engine', 'handlebars');
+
+app.use(express.static(__dirname + '/public'));
 app.use('/api/products', products);
 app.use('/api/carts', carts);
+app.use('/', views);
 
-app.listen(8080, () => console.log('El servidor inició en el puerto 8080'));
+const httpServer = app.listen(8080, () => console.log('El servidor inició en el puerto 8080'));
+
+const io = new Server(httpServer);
+
+io.on('connection',socket => {
+    console.log('Cliente conectado');
+    socket.on('message', data =>{
+        console.log(data);
+    })
+})
